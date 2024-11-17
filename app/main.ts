45e740048135b32cdea6eca0d5e84bc8171eb756
Rajat Ahuja <rahuja445@gmail.com>
2024-11-16 21:15:57 -0500

    add behavior

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,13 @@
-import * as net from 'net';
+import * as net from 'net'; // net module allows you to create TCP servers
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log('Logs from your program will appear here!');
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
+  connection.on('data', () => {
+    connection.write('+PONG\r\n');
+  });
   // Handle connection
 });
 
-server.listen(6379, '127.0.0.1');
+server.listen(6379, '127.0.0.1'); // 6379 is the default port that Redis uses
