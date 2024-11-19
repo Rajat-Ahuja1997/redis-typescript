3f121d2d4ac17b607de55bb4fb815b8442d23d9c
Rajat Ahuja <rahuja445@gmail.com>
2024-11-19 13:53:22 -0500

    handle dir and dbfilename

--- app/main.ts
+++ app/main.ts
@@ -3,6 +3,12 @@ import RESPParser, { type RESPData } from './parser';
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
   const map = new Map<string, string>();
+  const args = process.argv;
+  const parameters = args.slice(2);
+  const dir = parameters[parameters.indexOf('--dir') + 1];
+  const dbfilename = parameters[parameters.indexOf('--dbfilename') + 1];
+  map.set('dir', dir);
+  map.set('dbfilename', dbfilename);
   const parser = new RESPParser();
   connection.on('data', (data: Buffer) => {
     const parsedInput = parser.parse(data);
@@ -19,11 +25,6 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
   });
 });
 
-const args = process.argv;
-const parameters = args.slice(2);
-const dir = parameters[parameters.indexOf('--dir') + 1];
-const dbfilename = parameters[parameters.indexOf('--dbfilename') + 1];
-console.log(`dir: ${dir}, dbfilename: ${dbfilename}`);
 server.listen(6379, '127.0.0.1');
 function handleParsedInput(
   parsedInput: RESPData | null,
@@ -39,6 +40,7 @@ function handleParsedInput(
 
   if (parsedInput.type === '*' && Array.isArray(parsedValue)) {
     const command = (parsedValue[0] ?? '').toString().toUpperCase();
+    console.log('command', command);
 
     switch (command) {
       case 'PING':
@@ -79,19 +81,34 @@ function handleParsedInput(
         return value ? `$${value.length}\r\n${value}\r\n` : '$-1\r\n';
       }
 
-      case 'CONFIG GET': {
-        const parameter = parsedValue[1]?.toString();
-        if (!parameter) {
-          return '-ERR invalid arguments\r\n';
+      case 'CONFIG': {
+        const nestedCommand = parsedValue[1]?.toString();
+        if (!nestedCommand) {
+          return '-ERR missing second argument for CONFIG\r\n';
         }
+        if (nestedCommand === 'GET') {
+          const parameter = parsedValue[2]?.toString();
+          if (!parameter) {
+            return '-ERR invalid arguments\r\n';
+          }
 
-        switch (parameter) {
-          case 'dir':
-            return '+OK\r\n$2\r\n10\r\n';
-          case 'dbfilename':
-            return '+OK\r\n$9\r\ndump.rdb\r\n';
-          default:
-            return '-ERR unsupported parameter\r\n';
+          switch (parameter) {
+            case 'dir':
+              //*2\r\n$3\r\ndir\r\n$16\r\n/tmp/redis-files\r\n
+              return `*2\r\n$3\r\ndir\r\n$${
+                map.get('dir')?.length
+              }\r\n${map.get('dir')}\r\n`;
+            case 'dbfilename':
+              return `*2\r\n$9\r\ndbfilename\r\n$${
+                map.get('dbfilename')?.length
+              }\r\n${map.get('dbfilename')}\r\n`;
+            default:
+              console.log('unsupported parameter', parameter);
+              return '-ERR unsupported parameter\r\n';
+          }
+        } else {
+          console.log('unsupported nested command', nestedCommand);
+          return '-ERR unsupported nested command for CONFIG\r\n';
         }
       }
 
